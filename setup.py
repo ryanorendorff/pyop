4fa2ed99bcd16dd02bf86bf3eb436c7a8a7fda98
Ryan Orendorff <ryan@rdodesigns.com>
2014-07-10 00:08:20 -0700

    Updated setup.py test function.
    Follows what is on the py.test webpage now.

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+import sys
 from setuptools.command.test import test as TestCommand
 
 class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = None
+
     def finalize_options(self):
         TestCommand.finalize_options(self)
         self.test_args = []
@@ -11,7 +18,7 @@ class PyTest(TestCommand):
     def run_tests(self):
         #import here, cause outside the eggs aren't loaded
         import pytest
-        errno = pytest.main(self.test_args)
+        errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
 
