75f392d85def281036aba88dfb94e15a1a80097a
Ryan Orendorff <ryan@rdodesigns.com>
2014-08-10 23:06:46 -0700

    Add tests fftshift, ifftshift.
    Make sure the operators work on some random input. Some additional tests
    to make sure they throw errors when possible.

--- tests/fft_test.py
+++ tests/fft_test.py
@@ -78,3 +78,76 @@ def testIfftRandom():
         np.testing.assert_allclose(
             np.reshape(F._forward(np.ravel(arr, order)), s, order),
             np.fft.ifftn(arr, s = s))
+
+
+#####################
+#  FFT Shift Tests  #
+#####################
+
+def randomAxes(ndim):
+    return tuple(random.randint(0, ndim)
+        for _ in range(random.randint(0, ndim + 2)))
+
+def testFftshiftInputErrors():
+
+    with pytest.raises(ValueError):
+        operators.fftshift((8, -8), (1,))
+
+    with pytest.raises(ValueError):
+        operators.fftshift((8, 8), (-1,))
+
+    with pytest.raises(ValueError):
+        operators.fftshift((8, 8), (2, ))
+
+
+def testFftshiftRandom():
+    for _ in range(num_tests):
+        d = random.randint(1, dimensions_max + 1)
+
+        arr = np.random.rand(*tuple(
+            random.randint(1, array_max_size + 1) for _ in range(d)))
+
+        order = random.choice(('C', 'F'))
+
+        axes = random.choice((None, randomAxes(arr.ndim - 1)))
+
+        F = operators.fftshift(arr.shape, axes, order = order)
+
+        pyop.adjointTest(F)
+
+        np.testing.assert_allclose(
+            np.reshape(F._forward(np.ravel(arr, order)), arr.shape, order),
+            np.fft.fftshift(arr, axes))
+
+
+def testIfftshiftInputErrors():
+
+    with pytest.raises(ValueError):
+        operators.ifftshift((8, -8), (1,))
+
+    with pytest.raises(ValueError):
+        operators.ifftshift((8, 8), (-1,))
+
+    with pytest.raises(ValueError):
+        operators.ifftshift((8, 8), (2, ))
+
+
+def testIfftshiftRandom():
+    for _ in range(num_tests):
+        d = random.randint(1, dimensions_max + 1)
+
+        arr = np.random.rand(*tuple(
+            random.randint(1, array_max_size + 1) for _ in range(d)))
+
+        order = random.choice(('C', 'F'))
+
+        axes = random.choice((None, randomAxes(arr.ndim - 1)))
+
+        F = operators.ifftshift(arr.shape, axes, order = order)
+
+        pyop.adjointTest(F)
+
+        np.testing.assert_allclose(
+            np.reshape(F._forward(np.ravel(arr, order)), arr.shape, order),
+            np.fft.ifftshift(arr, axes))
+
